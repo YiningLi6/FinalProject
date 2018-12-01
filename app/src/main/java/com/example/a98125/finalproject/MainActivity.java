17590ccaf9a2ef20476b0ec07f5514ad93445ef6
Yining Li <yiningl6@illinois.edu>
2018-12-01 13:12:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/a98125/finalproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.a98125.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.enter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //API???
+                //here we got access to the textview
+                TextView textView = (TextView) findViewById(R.id.outputDisplay);
+                //???
+                textView.setText("We haven't finished this app yet!");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/a98125/finalproject/UI.java
@@ -0,0 +1,5 @@
+package com.example.a98125.finalproject;
+
+public class UI {
+
+}
